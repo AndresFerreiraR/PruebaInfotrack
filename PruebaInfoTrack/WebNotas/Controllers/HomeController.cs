f789ebf53d96726ead390b003217cd78cd434b47
AferreiraR <ac.ferreira.r@gmail.com>
2019-02-01 14:43:19 -0500

    Nueva Capa
    Nueva Capa

---
+++ PruebaInfoTrack/WebNotas/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TST = PruebaInfoTrack;
+
+namespace WebNotas.Controllers
+{
+    public class HomeController : Controller
+    {
+        TST.Controllers.EstudianteController EstudianteController = new TST.Controllers.EstudianteController();
+        public ActionResult Index()
+        {
+            var estudiante = EstudianteController.GetAllStudents();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+                       
+            return View();
+        }
+    }
+}
---
+++ PruebaInfoTrack/WebScore/wwwroot/js/Scripts/HomeScripts.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    HomeScripts.GetData();
+});
+
+class HomeScripts {
+
+    static GetData() {
+        debugger;
+        return $.ajax({
+            type: 'GET',
+            url: 'http://localhost:54691/api/Estudiante/ListarEstudiantes',
+            headers: { 'Content-Type': 'application/json', 'Authorization': 'Basic YWRtaW46YWRtaW4=', 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS', 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Origin': '*' },
+            contentType: 'application/json',
+            dataType: 'json',
+            success: function (response) {
+                var name = response;
+                var yourTableHTML = "";
+                jQuery.each(name, function (i, data) {
+                    $("#DataTable").append("<tr><td>" + data + "</td></tr>");
+                });
+            }
+        });
+    }
+}
